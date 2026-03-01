7a6c6617d591a13ec7fa7e2a5ca88b71421137e9
shubhamjh15 <shubhamjh175@gmail.com>
2026-03-01 20:29:20 +0530

    callback

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -8,7 +8,7 @@ export async function GET(request: Request) {
     // if "next" is in param, use it as the redirect URL
     const next = searchParams.get('next') ?? '/'
 
-    if (code) {
+    if (code) {   
         const cookieStore = await cookies()
         const supabase = createServerClient(
             process.env.NEXT_PUBLIC_SUPABASE_URL!,
